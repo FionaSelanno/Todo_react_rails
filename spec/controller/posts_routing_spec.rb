dd017e35eef215d97f17bf538a1f8bb4b4da908e
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-08 09:47:07 +0200

    added test routing index and create

--- spec/controller/posts_routing_spec.rb
+++ spec/controller/posts_routing_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :routing do
+RSpec.describe TodosController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-     expect(:get => "/").to route_to("posts#index")
+     expect(:get => "/todos").to route_to("todos#index")
+   end
 
+   it "routes to #create" do
+     expect(:post => "/todos").to route_to("todos#create")
    end
  end
+end
